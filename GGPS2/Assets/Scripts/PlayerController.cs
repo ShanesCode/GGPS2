80c8f27e3d42d226c35e6cdea22f0a65d85cd884
villaindotlove <scot@villain.love>
2021-07-12 13:45:44 +0100

    Add player-controller/create branch

---
+++ GGPS2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private float gravity;
+    private float friction;
+    private float drag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
