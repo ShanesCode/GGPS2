b518c3e37148d35a368067fd9041dd7234809271
Joshua <joshs2556@gmail.com>
2021-07-13 06:38:05 +0100

    Tilemap not attached to tile sprites yet idk if it was nt working because my mouse was dying or what. Door script not done some weird errors. Also I pressed some buttons on sourcetree and all the stuff turned red hopefully i didnt nuke it idk

---
+++ GGPS2/Assets/ExitDoor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D Collider2D)
+    {
+
+        Debug.Log("You sure you can fit through this fatty?");
+
+    }
+
+}
