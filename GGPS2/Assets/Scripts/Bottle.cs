b510ffc6291f4c4d12ad6540cd048fc3a0a21eb8
nollymax <njjhayward@gmail.com>
2021-07-14 05:10:51 +0100

    Moved bottle code out of player controller script

--- GGPS2/Assets/Scripts/Bottle.cs
+++ GGPS2/Assets/Scripts/Bottle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bottle : MonoBehaviour
 {
+    public GameObject bottle;
+    public GameObject bin;
+    public bool hasBottle;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,10 @@ public class Bottle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            PlaceBottle();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -37,6 +43,31 @@ public class Bottle : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             Debug.Log("Beep Boop Exit");
+        }
+    }
+    void PlaceBottle()
+    {
+        print(hasBottle);
+        print(bin.transform.position.x);
+        print(transform.position.x);
+        if (hasBottle == false)
+        {
+            Instantiate(bottle, transform.position + new Vector3(2, 0, 0), transform.rotation);
+        }
+        if (hasBottle == true)
+        {
+            // Calculate distance between player and bin objects
+            float diff = transform.position.x - bin.transform.position.x;
+            if (diff < 2 && diff > -2)
+            {
+                print("this is working");
+                hasBottle = false;
+            }
+            else
+            {
+                hasBottle = false;
+                Instantiate(bottle, transform.position + new Vector3(2, 0, 0), transform.rotation);
+            }
         }
     }
 }
