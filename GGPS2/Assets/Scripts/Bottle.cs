b469d40cb2181f091a04ee12e15232d6c34499da
nollymax <njjhayward@gmail.com>
2021-07-14 20:51:39 +0100

    Disabled Kinematic toggle pending Scot's rigidbody update

--- GGPS2/Assets/Scripts/Bottle.cs
+++ GGPS2/Assets/Scripts/Bottle.cs
@@ -8,16 +8,16 @@ public class Bottle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector3(0, -0.01f, 0);
+        //GetComponent<Rigidbody2D>().velocity = new Vector3(0, -0.01f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<Rigidbody2D>().velocity.y == 0)
-        {
-            GetComponent<Rigidbody2D>().isKinematic = true;
-        }
+        //if (GetComponent<Rigidbody2D>().velocity.y == 0)
+        //{
+        //    GetComponent<Rigidbody2D>().isKinematic = true;
+        //}
     }
 
     private void OnTriggerStay2D(Collider2D collision)
