5038ce37f450f883fad4fa3c26c79ac55c4fd830
Shane Massaad <shanemassaad@hotmail.com>
2021-07-15 15:51:09 +0100

    Add music controller and fix volume controls

--- GGPS2/Assets/Scripts/SettingsManager.cs
+++ GGPS2/Assets/Scripts/SettingsManager.cs
@@ -14,16 +14,19 @@ public class SettingsManager : MonoBehaviour
     public Slider sfxVolume;
 
     // Start is called before the first frame update
+    void Awake()
+    {
+        masterVolume.onValueChanged.AddListener(SetMasterVolume);
+        musicVolume.onValueChanged.AddListener(SetMusicVolume);
+        sfxVolume.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    // Start is called before the first frame update
     void Start()
     {
         masterVolume.value = PlayerPrefs.GetFloat("masterVolume", 0.5f);
-        masterVolume.onValueChanged.AddListener(SetMasterVolume);
-
         musicVolume.value = PlayerPrefs.GetFloat("musicVolume", 0.5f);
-        musicVolume.onValueChanged.AddListener(SetMusicVolume);
-
         sfxVolume.value = PlayerPrefs.GetFloat("sfxVolume", 0.5f);
-        sfxVolume.onValueChanged.AddListener(SetSFXVolume);
     }
 
     // Update is called once per frame
@@ -38,21 +41,28 @@ public class SettingsManager : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("masterVolume", masterVolume.value);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume.value);
+        PlayerPrefs.SetFloat("sfxVolume", sfxVolume.value);
+    }
+
     public void SetMasterVolume(float sliderValue)
     {
-        mixer.SetFloat("masterVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("masterVolume", Mathf.Log10(sliderValue) * 30);
         PlayerPrefs.SetFloat("masterVolume", sliderValue);
     }
 
     public void SetMusicVolume(float sliderValue)
     {
-        mixer.SetFloat("musicVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("musicVolume", Mathf.Log10(sliderValue) * 30);
         PlayerPrefs.SetFloat("musicVolume", sliderValue);
     }
 
     public void SetSFXVolume(float sliderValue)
     {
-        mixer.SetFloat("sfxVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("sfxVolume", Mathf.Log10(sliderValue) * 30);
         PlayerPrefs.SetFloat("sfxVolume", sliderValue);
     }
 }
