756761ecd811c59ddf3eb8a7e1b54388a658ca40
Shane Massaad <shanemassaad@hotmail.com>
2021-07-18 14:58:58 +0100

    Replace ground check function with trigger to check for ground

---
+++ GGPS2/Assets/GroundedTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedTrigger : MonoBehaviour
+{
+    public LayerMask groundMask;
+    public LayerMask bottleMask;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Bottle")
+        {
+            player.GetComponent<PlayerController>().grounded = true;
+            player.GetComponent<PlayerController>().ground = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bottle" || collision.gameObject.tag == "Bottle")
+        {
+            player.GetComponent<PlayerController>().grounded = true;
+            player.GetComponent<PlayerController>().ground = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Bottle")
+        {
+            player.GetComponent<PlayerController>().ground = null;
+            player.GetComponent<PlayerController>().grounded = false;
+        }
+    }
+}
